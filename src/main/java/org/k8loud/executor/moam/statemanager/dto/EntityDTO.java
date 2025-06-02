1e2b58cc992ec5ce853ddd6bd195a5a70bbb3724
proman3419 <proman7268@gmail.com>
2025-06-02 16:42:10 +0200

    EntityDTO annotations

--- src/main/java/org/k8loud/executor/moam/statemanager/dto/EntityDTO.java
+++ src/main/java/org/k8loud/executor/moam/statemanager/dto/EntityDTO.java
@@ -1,7 +1,13 @@
 package org.k8loud.executor.moam.statemanager.dto;
 
 import com.fasterxml.jackson.databind.JsonNode;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
 
+@Setter
+@Getter
+@ToString(exclude = "definition")
 public class EntityDTO {
     public int id;
     public String apiVersion;
